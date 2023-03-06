2efc41f41b60b6fa62b14cc285f28cd6dab5984f
suraj kumar <suraj.kumar@knorex.com>
2023-03-06 12:25:08 +0530

    first commit

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,8 @@
 import io from "socket.io-client";
-export const ENDPOINT = "https://backend-polling.vercel.app"
+export const ENDPOINT = window.location.hostname === 'localhost'
+  ? 'http://localhost:5000'
+  : 'https://your-production-url.com';
+
 
 
 const socket = io(ENDPOINT, {
